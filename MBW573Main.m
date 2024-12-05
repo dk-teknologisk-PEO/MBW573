b8284c1f54778fe85a3dc7a3a0365634dae5d9a2
Emil Andreasen Klahn <eak@teknologisk.dk>
2024-12-05 14:59:03 +0100

    move control from virtual machine

--- MBW573Main.m
+++ MBW573Main.m
@@ -2,19 +2,17 @@ clear variables
 close all
 clc
 
-
-[MBWsettings, MBWAddress, MBWPort] = MBW573ReadSetupFile("C:\Users\peo\Documents\GitHub\materialefugt\");
+[MBWsettings, MBWAddress, MBWPort] = MBW573ReadSetupFile("C:\materialefugt\materialefugt\");
 [MBW573Table] = MBW573CreateTable();
 [t, model] = MBW573Initialize(MBWAddress,MBWPort);
 MBW573SetupInstrument(t,MBWsettings)
 
-
-tStart=now;
+tStart=datetime('now');
 while i<60
-    tNow=now;
+    tNow=datetime('now');
     deltaT=(tNow-tStart)*24*60*60;
     if deltaT>1
-        MBW573Table = MBW573Read(t,MBW573Table)
+        MBW573Table = MBW573Read(t,MBW573Table);
         tStart=tStart+1/(24*60*60);
         i=i+1;
     end  
